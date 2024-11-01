fbeca38595293542b41568bb900462b80e09b170
DarshanMistari <mistaridarshan2@gmail.com>
2024-11-01 23:05:48 +0530

    [ADDED]:Darshan - Python code for Arithmatic operation

---
+++ Arithmatic Operation.py
@@ -0,0 +1,23 @@
+# Arithmatic Operation
+
+Num1 = int(input("Enter the First Number :"))
+Num2 = int(input("Enter the Second Number :"))
+
+Sum = Num1 + Num2
+print("Addition :",Sum)
+
+Sub = Num1 - Num2
+print("Subtraction :",Sub)
+
+Mul = Num1 * Num2
+print("Multiplication :",Mul)
+
+Div = Num1 / Num2
+print("Division :",Div)
+
+#integer Division output
+Div1 = Num1 // Num2
+print("Division :", Div1)
+
+Mod = Num1 % Num2
+print("Modulas :" ,Mod)
