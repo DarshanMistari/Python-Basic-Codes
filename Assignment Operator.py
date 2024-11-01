0f87d43dc0f9e68b0cf02026e16571b5b55807bf
DarshanMistari <mistaridarshan2@gmail.com>
2024-11-01 23:06:43 +0530

    [ADDED]:Darshan - Python code for Assignment operation

---
+++ Assignment Operator.py
@@ -0,0 +1,18 @@
+#Assignment Operator
+
+num1 = 10
+
+num2 = num1
+print("Num1 is Assign Num2 :",num2)
+
+num1 += 5
+print(num1)
+
+num2 /= 2
+print(num2)
+
+num1 *= 5
+print(num1)
+
+num2 **=2
+print(num2)
