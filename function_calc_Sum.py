c50d70a6ebf75b313480cc5b36dadb2e35d3be30
DarshanMistari <mistaridarshan2@gmail.com>
2024-11-07 00:16:01 +0530

    [ADDED]:Darshan - Python code function calc sum

---
+++ function_calc_Sum.py
@@ -0,0 +1,12 @@
+# Using Function Calculate the Sum
+
+def calc_Sum(a , b):
+    sum = a + b
+    print("\n Sum is :",sum)
+    return sum
+
+# First Time Call
+calc_Sum(5, 10)
+
+# Second Time Call
+calc_Sum(15, 15)
