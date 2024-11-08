4bb1ddaf05b44d2576c220ebde90ef73e44cb5b1
DarshanMistari <mistaridarshan2@gmail.com>
2024-11-09 00:26:12 +0530

    [ADDED]:Darshan - Python list

---
+++ List in Python.py
@@ -0,0 +1,9 @@
+#List in Python
+
+marks =[56, 67, 33, 89, 45.3, 33.8, "A", "Darshan", "Kalpesh"]
+
+print("Print List :",marks)
+print("List Lenght :",len(marks))
+print("Acesing List in Index :",marks[4])
+print("List 7 Index :", marks[7])
+print("4 Num Marks :",marks[3])
