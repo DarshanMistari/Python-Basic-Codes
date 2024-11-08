4d6472d4597350ff5c8c7b83aed07f98c37a0dae
DarshanMistari <mistaridarshan2@gmail.com>
2024-11-09 00:24:57 +0530

    [ADDED]:Darshan - Python code input info

---
+++ input Info.py
@@ -0,0 +1,6 @@
+Name = input("Enter Your Name :")
+Age = input("Enter Your Age :")
+Fees = int(input("Enter the College Fees :"))
+
+print("My Name is", Name ,"and i am", Age ,"year old.")
+print("College Fees :", Fees)
