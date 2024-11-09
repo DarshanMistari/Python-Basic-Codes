9968fa9f84a333b1e2768ac3e11a360ad4fcb233
DarshanMistari <mistaridarshan2@gmail.com>
2024-11-10 00:17:05 +0530

    [ADDED]:Darshan - Python logical operator

---
+++ Logical Operator.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter the First Number:"))
+num2 = int(input("Enter the Second Number:"))
+
+print("And Oprator,Both Cond True :", (num1 == num2) and (num1 >= num2))
+print("OR Operator, Only one Cond True :", (num1 == num2) or (num1 >= num2))
+print("Not Operator,True cond False ,F cond T :", (num1 != num2 ))
