1c68b8f16ef4ec3092c55b642bbfc26be116d48d
DarshanMistari <mistaridarshan2@gmail.com>
2024-11-11 18:16:11 +0530

    [ADDED]:Darshan - Python code

---
+++ Relational Operator.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the First Number:"))
+num2 = int(input("Enter the Second Number:"))
+
+print("Num1 and Num2 is Equal :",num1 == num2)
+print("Num1 and Num2 is Not Equal :",num1 != num2)
+print("Num1 is greater than Equal to Num2 :",num1 >= num2)
+print("Num1 is Less than Equal to Num2 :",num1 <= num2)
+print("Num1 is greater than Num2 :",num1 > num2)
+print("Num1 is Less than Num2 :",num1 < num2)
