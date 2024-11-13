54a3664e3a59068d7b4fff36ef3ff535f5ea337a
DarshanMistari <mistaridarshan2@gmail.com>
2024-11-13 19:51:22 +0530

    [ADDED]:Darshan - Python code

---
+++ String Function.py
@@ -0,0 +1,15 @@
+Name = "Python is easy Language"
+
+print("String Length :",len(Name))
+
+print("String Capitalize :",Name.capitalize())
+
+print("String Char Count :",Name.count("a"))
+
+print("String Leplace :",Name.replace( "Python is easy Language", "Python is Case Sensitive Language"))
+
+print("String find :",Name.find("easy"))
+
+print("String Startwith :",Name.startswith("Py"))
+
+print("String Endwith :",Name.endswith("age"))
