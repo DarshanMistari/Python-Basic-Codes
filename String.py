5489228cfbb7256d78bca5274e131dd23b04d90b
DarshanMistari <mistaridarshan2@gmail.com>
2024-11-13 19:52:33 +0530

    [ADDED]:Darshan - Python code

---
+++ String.py
@@ -0,0 +1,19 @@
+Name = "Hello EveryOne"
+Name1 = 'I am Darshan Mistari'
+Name2 = """Today are Learn Python"""
+
+print("1.TypenString Define :" , Name)
+print("2.TypenString Define :" , Name1)
+print("3.TypenString Define :" , Name2)
+
+# String Basic Functions
+
+print("String UpperCase :",Name.upper())
+
+print(Name1.lower())
+
+print("String End With Character :",Name.endswith("e"))
+
+print("Find Characte or Name :",Name1.find("Darshan"))
+
+print("String Start With :",Name2.startswith("Today"))
