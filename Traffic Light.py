4291491473baddd335a45ced84b9ed35ccac4d16
DarshanMistari <mistaridarshan2@gmail.com>
2024-11-14 22:13:36 +0530

    [ADDED]:Darshan - Python code

---
+++ Traffic Light.py
@@ -0,0 +1,13 @@
+light = input("Enter Traffic light Color :")
+
+if(light == "red"):
+    print("Car is Stop.")
+    
+elif(light == "green"):
+    print("Car is Go On Drive.")
+    
+elif(light == "yellow") :
+    print("Car is Look the Singnal")  
+    
+else:
+    print("Light is Broken") 
