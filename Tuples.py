55eccd66b7e19f0241fd035e6692245aa74581e9
DarshanMistari <mistaridarshan2@gmail.com>
2024-11-15 00:28:28 +0530

    [ADDED]:Darshan - Python code

---
+++ Tuples.py
@@ -0,0 +1,19 @@
+tuple = (123,22,51,56,34,)
+
+tuple1=()
+print("Type in Tuple :",type(tuple1))
+
+tuple2 =(1,)
+print("Tuple Difined :",tuple2)
+
+#Accessing in Index tupel Value
+print("Accesing the Tuple Value :",tuple)
+
+print("Tuple accesing in index :",tuple[1])
+
+print("Tuple accesing in index :",tuple[2])
+
+print("Tuple accesing in index :",tuple[4])
+
+
+
