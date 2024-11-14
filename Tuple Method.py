195923bbf21b36be237f4ee42edea5e39c1fce31
DarshanMistari <mistaridarshan2@gmail.com>
2024-11-15 00:27:53 +0530

    [ADDED]:Darshan - Python code

---
+++ Tuple Method.py
@@ -0,0 +1,11 @@
+#Tuple Method
+
+Marks = (56,45.56,56,34,68,45.7,56,91)
+
+Marks1 = Marks.count(56);
+
+print("Conut the Index Tuple :",Marks1)
+
+Marks2 =  Marks.index(91)
+
+print("Marks 91 which index :",Marks2)
