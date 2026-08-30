90e1f5c0c38847fc159864cdbd53f83b874ac6d1
DarshanMistari <mistaridarshan2@gmail.com>
2026-08-30 21:58:15 +0530

    [ADDED]:Darshan - data types in python

---
+++ data_Types.py
@@ -0,0 +1,9 @@
+name = "Darshan"
+age = 22
+gender = "Male"
+is_Student = True
+
+print("Your name is :",name)
+print("Your age is :",age)
+print("Gender is :",gender)
+print("You are Student :",is_Student)
