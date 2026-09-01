68f651c308415eee32272d0732525df8e5835482
DarshanMistari <mistaridarshan2@gmail.com>
2026-09-01 21:53:17 +0530

    [ADDED]:Darshan - Type Conversion

---
+++ type_Conversion.py
@@ -0,0 +1,15 @@
+# Explicit Conversion
+
+num1 = "100" 
+num2 = "200" 
+
+num3= int(num1) 
+num4 = int(num2)
+
+num5 = "23.45"
+num6 = float(num5)
+
+print("Addition :",num1 + num2 ,sep=" ") # 
+print("Addition :",num3 + num4)
+print("Num6 :",num6)
+
