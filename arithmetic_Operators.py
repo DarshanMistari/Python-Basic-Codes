92b22fff4479aa7eb286c5b5c8a3c6b75d229db1
DarshanMistari <mistaridarshan2@gmail.com>
2026-09-01 21:46:46 +0530

    [ADDED]:Darshan - Arithmetic operator

---
+++ arithmetic_Operators.py
@@ -0,0 +1,13 @@
+# Arithmetic Operation 
+
+num1 = int(input("Enter the First Number :"))
+num2 = int(input("Enter the Second Number :"))
+
+print("\nAddition : ",num1 + num2)
+print("Subtraction :",num1 - num2)
+print("Multiplication :",num1 * num2)
+print("Division :",num1 / num2)
+print("Modulus :",num1 % num2)
+print("Floor Division :",num1 // num2)
+print("Exponentiation:",num1 ** num2)
+
