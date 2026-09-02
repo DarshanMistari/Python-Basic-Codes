7a72d3951373d6a7d8ac4befd761256566cc7f9b
DarshanMistari <mistaridarshan2@gmail.com>
2026-09-02 21:59:49 +0530

    [ADDED]:Darshan - Escape sequences in python

---
+++ escape_Sequenceas.py
@@ -0,0 +1,13 @@
+"""
+\n - New line
+\t - Tab space
+\\ - Backslash
+\" - Double Quotes
+\' - Single Quotes
+"""
+
+print("Hello everyone \n How are you ?")  
+print("Hello everyone \t How are you ?")
+print("Hello everyone \\ How are you ?")
+print("My Name is \"Darshan Mistari\"")
+print('My Name is \'Darshan Mistari\'')
