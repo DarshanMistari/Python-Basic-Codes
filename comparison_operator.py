828e85120def4c6146c8aa26c1fde52a834e120d
DarshanMistari <mistaridarshan2@gmail.com>
2026-09-03 21:27:51 +0530

    [ADDED]:Darshan - Comparison Operator

---
+++ comparison_operator.py
@@ -0,0 +1,10 @@
+# Comparison operator
+num1 = 40
+num2 = 20
+
+print("Num1 is equal to Num2 :",num1 == num2)
+print("Num1 is Greater than Num2 :",num1 > num2)
+print("Num1 is Less than Num2 :",num1 < num2)
+print("Num1 Greater than Eqaul to Num2 :",num1 >= num2)
+print("Num1 Less than Eqaul to Num2 :",num1 <= num2)
+print("Num1 Not eqaul to Num2",num1 != num2)
