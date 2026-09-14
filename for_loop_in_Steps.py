81124fc9268fa5f10ad281fe670f124257cffb85
DarshanMistari <mistaridarshan2@gmail.com>
2026-09-14 10:22:14 +0530

    [ADDED]:Darshan - for loop in step

---
+++ for_loop_in_Steps.py
@@ -0,0 +1,7 @@
+# Steps in For loop 
+
+# 1 3 5 7 9 print the number's, Every 2 Num Difference
+
+# First Num is Start value, Second Number is end Value -1,last num is Difference 
+for i in range(1,11,2): 
+    print(i ,end=" ") 
