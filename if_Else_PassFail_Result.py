218cf2b94438fb7f1e7e9ace14c1a07a22d2b474
DarshanMistari <mistaridarshan2@gmail.com>
2026-09-23 21:04:39 +0530

    [ADDED]:Darshan - if else pass fail result

---
+++ if_Else_PassFail_Result.py
@@ -0,0 +1,8 @@
+
+java = int(input("Enter your java Marks :"))
+python = int(input("Enter your python Marks :"))
+
+if java >= 24 and python >= 24:
+    print("You are Pass, Congratulation")
+else:
+    print("You are Fail, Please Try Agains")
