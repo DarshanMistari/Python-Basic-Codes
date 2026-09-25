057ed3a41f7cb5c5fee4d4c44fa936ab09714cb8
DarshanMistari <mistaridarshan2@gmail.com>
2026-09-25 22:07:18 +0530

    [ADDED]:Darshan - for loop user input

--- for_Loop_user_Input.py
+++
@@ -1,7 +0,0 @@
-# Take a users input start and ending num, using for loop.
-
-start = int(input("Enter the Start Number :"))
-end = int(input("Enter the End Number :")) 
-
-for i in range(start, end + 1): 
-    print(i , end=" ")
